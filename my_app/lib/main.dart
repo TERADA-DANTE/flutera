a47b9da0ed4ed5a34536903bd858e868cd77a118
Matsunaga syun <terada.dante@gmail.com>
2021-01-09 21:36:46 +0900

    :hammer: Update main.dart
    Update homepage child

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -6,11 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // 앱 자체의 메타? 이므로 스마트폰등에서 이것을 참조한다.
-        title: 'My first flutter app',
-        theme: ThemeData(primarySwatch: Colors.blue), // ThemeData
-        // home.isRequired~~
-        home: MyHomePage());
+      title: "Flutera",
+      home: MyHomePage(),
+    );
   }
 }
 
@@ -19,12 +17,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // 앱 화면이므로 Text 클래스를 가져온다.
-        title: Text('Hello world!'),
-      ),
-      body: Center(
-        child: Column(
-            children: <Widget>[Text('Terada'), Text('Soeda'), Text('Okimary')]),
+        title: Text("Hello world"),
       ),
     );
   }
