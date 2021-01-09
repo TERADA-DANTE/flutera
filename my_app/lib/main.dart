905676d238f6d1c66e7874565271bb5679994ee8
Matsunaga syun <terada.dante@gmail.com>
2021-01-09 13:38:51 +0900

    Create Scaffold : App header

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -6,8 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        // 앱 자체의 메타? 이므로 스마트폰등에서 이것을 참조한다.
         title: 'My first flutter app',
-        theme: ThemeData(primarySwatch: Colors.blue),
+        theme: ThemeData(primarySwatch: Colors.blue), // ThemeData
+        // home.isRequired
         home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // 앱 화면이므로 Text 클래스를 가져온다.
+        title: Text('Hello world!'),
+      ),
+      body: Center(
+        child: Column(children: <Widget>[
+          Text('Terada'),
+          Text('Soeda'),
+          Text('Okimaryr')
+        ]),
+      ),
+    );
   }
 }
