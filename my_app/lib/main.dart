9fc037b172f9943855ea800344a3bcfaafff7c13
Matsunaga syun <terada.dante@gmail.com>
2021-01-12 22:15:22 +0900

    :building_construction: Update main.dart and create components.dart
    2 pages

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Flutera());
+
+class Flutera extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "flutera",
+      initialRoute: "/",
+      routes: {"/": (context) => Home(), "/detail": (context) => Detail()},
+    );
+  }
+}
